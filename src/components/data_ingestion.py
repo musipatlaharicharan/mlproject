e454edb52b1c463aa9a3ccb60f1e87a2f67c7e55
m.haricharan <musipatlaharicharan2005@gmail.com>
2026-03-23 10:42:14 +0530

    Fix data ingestion bugs (pandas import, data paths, syntax)

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,7 +2,7 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-import pandas as pandas
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 @dataclass
@@ -13,22 +13,23 @@ class DataIngestionConfig:
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_congif=DataIngestionConfig()
+        self.ingestion_config=DataIngestionConfig()
     def initiate_data_ingestion(self):
         logging.info("entered the data ingestion meythod or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('stud.csv')
             logging.info("read the dataset as dataframe")
-            os.makedirs(os.path.dirname(self.ingestion_congif.train_data_path))
-            df.to_csv(self.ingestion_congif.raw_data_path,index=False,header=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            train_set.to_csv(self.ingestion_congif.train_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_congif.train_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             logging.info("inmgestion ofthe data is completed")
             return(
-                self.ingestion_congif.train_data_path
-                self.ingestion_congif.test_data_path
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
 
             )
         except Exception as e:
